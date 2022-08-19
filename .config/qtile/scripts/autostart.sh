857eb7f9f129b433af38d7165f3de9a78d88d1e6
Tony Ampomah <tony@arksolutions.it>
2022-08-19 23:08:46 +0100

    Update qtile autostart app

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -7,22 +7,10 @@ function run {
   fi
 }
 
-#change your keyboard if you need it
-#setxkbmap -layout be
-
 keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
-
-#Some ways to set your wallpaper besides variety or nitrogen
-feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
-feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
-#wallpaper for other Arch based systems
-#feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
-#start the conky to learn the shortcuts
-(conky -c $HOME/.config/qtile/scripts/system-overview) &
 
 #start sxhkd to replace Qtile native key-bindings
 run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
-
 
 #starting utility applications at boot time
 run variety &
@@ -37,14 +25,5 @@ picom --config $HOME/.config/qtile/scripts/picom.conf &
 
 #starting user applications at boot time
 run volumeicon &
-#run discord &
-#nitrogen --restore &
-#run caffeine -a &
-#run vivaldi-stable &
-#run firefox &
-#run thunar &
-#run dropbox &
-#run insync start &
-#run spotify &
-#run atom &
-#run telegram-desktop &
+run caffeine -a &
+run dropbox &
