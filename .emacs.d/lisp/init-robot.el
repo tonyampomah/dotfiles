b0e9c0ac4687e94dfa2f628b5856e1cec2222846
Tony Ampomah <tony@arksolutions.it>
2025-03-15 06:42:07 +0000

    Configuring Emacs for robot framework

--- .emacs.d/lisp/init-robot.el
+++ .emacs.d/lisp/init-robot.el
@@ -4,5 +4,67 @@
 (use-package robot-mode
   :defer t)
 
+(setq lsp-keymap-prefix "C-c l")  ;; Or any other preferred keybinding
+
+(defun lsp-robot-framework-server-command ()
+  "Return the command to start the Robot Framework Language Server."
+  (list "robotframework_ls"))
+
+(lsp-register-client
+ (make-lsp-client
+  :new-connection (lsp-stdio-connection #'lsp-robot-framework-server-command)
+  :major-modes '(robot-mode)
+  :server-id 'robotframework-ls))
+
+
+(defun find-robot-project-root ()
+  "Find the root directory of the Robot Framework project."
+  (or (locate-dominating-file default-directory "pyproject.toml")  ;; Change marker if needed
+      (locate-dominating-file default-directory "robot_tests")     ;; Alternative marker
+      (vc-root-dir)))  ;; Use version control root as fallback
+
+(defun run-robot-test-in-project ()
+  "Run the Robot Framework test case at point within the project root using a relative path."
+  (interactive)
+  (let* ((test-name (thing-at-point 'line t))  ;; Get current test name
+         (test-name (string-trim test-name))   ;; Trim whitespace
+         (file-name (buffer-file-name))        ;; Get absolute path of test file
+         (project-root (find-robot-project-root))  ;; Find project root
+         (relative-file (file-relative-name file-name project-root)))  ;; Get relative path
+    (unless project-root
+      (error "Project root not found!"))
+    (let ((default-directory project-root))  ;; Set working directory
+      (compile (format "robot --test \"%s\" \"%s\"" test-name relative-file)))))
+
+(global-set-key (kbd "C-c r") 'run-robot-test-in-project)  ;; Shortcut to run a test
+
+(defun run-robot-file-in-project ()
+  "Run the Robot Framework test case file."
+  (interactive)
+  (let* (
+         (file-name (buffer-file-name))        ;; Get absolute path of test file
+         (project-root (find-robot-project-root))  ;; Find project root
+         (relative-file (file-relative-name file-name project-root)))  ;; Get relative path
+    (unless project-root
+      (error "Project root not found!"))
+    (let ((default-directory project-root))  ;; Set working directory
+      (compile (format "robot \"%s\"" relative-file)))))
+
+(defun run-robot-project ()
+  "Run the Robot Framework test case within an entire tests directory."
+  (interactive)
+  (let* (
+         (project-root (find-robot-project-root))  ;; Find project root
+         )  ;; Get relative path
+    (unless project-root
+      (error "Project root not found!"))
+    (let ((default-directory project-root))  ;; Set working directory
+      (compile (format "robot tests")))))
+
+(kd/my-local-leader-def'normal robot-mode-map
+			       "tt" 'run-robot-test-in-project
+			       "tf" 'run-robot-file-in-project
+			       "tp" 'run-robot-project)
+
 (provide 'init-robot)
 ;;; init-robot.el ends here
