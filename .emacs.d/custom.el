39ef88936dc47ed799c1bfdbf309fa86a5810426
Tony Ampomah <tony@arksolutions.it>
2025-07-19 19:01:19 +0100

    Various updates

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,10 +4,19 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("1f82b7df8a4ce0e2ba3b0217d2c552b2fe5b5dd1244481cb65bef733f9818c63" "47db66f76b42ba84674ba5c0ff9aaefbaa9425e597538cfa26f0f20b407cea1d" "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" "7b602fe4a324dc18877dde647eb6f2ff9352566ce16d0b888bfcb870d0abfd70" "d41229b2ff1e9929d0ea3b4fde9ed4c1e0775993df9d998a3cdf37f2358d386b" "712dda0818312c175a60d94ba676b404fc815f8c7e6c080c9b4061596c60a1db" "fbf73690320aa26f8daffdd1210ef234ed1b0c59f3d001f342b9c0bbf49f531c" default))
+   '("1f82b7df8a4ce0e2ba3b0217d2c552b2fe5b5dd1244481cb65bef733f9818c63"
+     "47db66f76b42ba84674ba5c0ff9aaefbaa9425e597538cfa26f0f20b407cea1d"
+     "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec"
+     "7b602fe4a324dc18877dde647eb6f2ff9352566ce16d0b888bfcb870d0abfd70"
+     "d41229b2ff1e9929d0ea3b4fde9ed4c1e0775993df9d998a3cdf37f2358d386b"
+     "712dda0818312c175a60d94ba676b404fc815f8c7e6c080c9b4061596c60a1db"
+     "fbf73690320aa26f8daffdd1210ef234ed1b0c59f3d001f342b9c0bbf49f531c" default))
  '(doom-modeline-check-simple-format t nil nil "Customized with use-package doom-modeline")
  '(org-agenda-files
-   '("/home/tonyampomah/org/calendar/scal.org" "/home/tonyampomah/org/calendar/ecal.org" "/home/tonyampomah/org/calendar/jcal.org" "/home/tonyampomah/org/calendar/pcal.org" "/home/tonyampomah/org/personal-inbox.org" "/home/tonyampomah/org/personal-todo.org" "/home/tonyampomah/org/personal-routine.org"))
+   '("/home/tonyampomah/org/goals.org" "/home/tonyampomah/org/inbox.org"
+     "/home/tonyampomah/org/init.org" "/home/tonyampomah/org/routine.org"
+     "/home/tonyampomah/org/someday.org" "/home/tonyampomah/org/todo.org"
+     "/home/tonyampomah/org/wishlist.org"))
  '(shell-pop-full-span t)
  '(shell-pop-shell-type '("vterm" "*vterm*" (lambda nil (vterm))))
  '(shell-pop-term-shell "/usr/zsh")
