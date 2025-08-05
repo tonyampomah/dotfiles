76557b1a1150b5251c48a7880ae86339d0c699a4
Tony Ampomah <tony@arksolutions.it>
2025-08-05 03:13:27 +0100

    Various tweaks

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("1f82b7df8a4ce0e2ba3b0217d2c552b2fe5b5dd1244481cb65bef733f9818c63"
+   '("fec5c912ae88d062ef2225f2552641759e9fa4c221a25f5210aae1fcc52f7c6f"
+     "7771c8496c10162220af0ca7b7e61459cb42d18c35ce272a63461c0fc1336015"
+     "1f82b7df8a4ce0e2ba3b0217d2c552b2fe5b5dd1244481cb65bef733f9818c63"
      "47db66f76b42ba84674ba5c0ff9aaefbaa9425e597538cfa26f0f20b407cea1d"
      "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec"
      "7b602fe4a324dc18877dde647eb6f2ff9352566ce16d0b888bfcb870d0abfd70"
