9be3fc26a6f95c569dc44c31379ae707c33526fd
Tony Ampomah <tony@arksolutions.it>
2025-10-20 20:37:03 +0100

    Enable Emacs auto dark or light theme based on system theme

--- .emacs.d/lisp/init-themes.el
+++ .emacs.d/lisp/init-themes.el
@@ -2,18 +2,37 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (straight-use-package 'doom-themes)
-;; (straight-use-package 'color-theme-sanityinc-tomorrow)
-(straight-use-package 'modus-themes)
-(straight-use-package 'doom-themes)
-;; (straight-use-package 'base16-theme)
 (straight-use-package 'catppuccin-theme)
 
-;; (load-theme 'modus-vivendi-deuteranopia t)
-(load-theme 'catppuccin t)
-(setq catppuccin-flavor 'latte) ;; or 'latte, 'macchiato, or 'mocha
-(catppuccin-reload)
+(use-package catppuccin-theme
+  :ensure t
+  :init
+  ;; Start with a default flavor
+  (setq catppuccin-flavor 'mocha)
+  :config
+  (load-theme 'catppuccin t))
 
+(use-package auto-dark
+  :ensure t
+  :custom
+  (auto-dark-themes '((catppuccin) (catppuccin)))
+  (auto-dark-polling-interval-seconds 5)
+  (auto-dark-allow-osascript nil)
+  (auto-dark-allow-powershell nil)
+  :hook
+  (auto-dark-dark-mode
+   . (lambda ()
+       (setq catppuccin-flavor 'mocha)
+       (catppuccin-reload)
+       (message "🌙 Catppuccin Mocha enabled")
+        ))
+  (auto-dark-light-mode
+   . (lambda ()
+       (setq catppuccin-flavor 'latte)
+       (catppuccin-reload)
+       (message "🌞 Catppuccin Latte enabled")
+        ))
+  :init (auto-dark-mode))
 
 (provide 'init-themes)
 ;;; init-themes.el ends here
