4d460e467356692634d4c113d2b1ceb373b71a6c
Tony Ampomah <tony@arksolutions.it>
2026-04-12 21:07:06 +0100

    Update perspective switch keybinding to super key instead of meta

--- .emacs.d/lisp/init-workspace.el
+++ .emacs.d/lisp/init-workspace.el
@@ -29,15 +29,15 @@
   "<tab>." 'persp-switch
   )
 
-(global-set-key (kbd "M-1") #'(lambda () (interactive) (persp-switch-by-number 1)))
-(global-set-key (kbd "M-2") #'(lambda () (interactive) (persp-switch-by-number 2)))
-(global-set-key (kbd "M-3") #'(lambda () (interactive) (persp-switch-by-number 3)))
-(global-set-key (kbd "M-4") #'(lambda () (interactive) (persp-switch-by-number 4)))
-(global-set-key (kbd "M-5") #'(lambda () (interactive) (persp-switch-by-number 5)))
-(global-set-key (kbd "M-6") #'(lambda () (interactive) (persp-switch-by-number 6)))
-(global-set-key (kbd "M-7") #'(lambda () (interactive) (persp-switch-by-number 7)))
-(global-set-key (kbd "M-8") #'(lambda () (interactive) (persp-switch-by-number 8)))
-(global-set-key (kbd "M-9") #'(lambda () (interactive) (persp-switch-by-number 9)))
+(global-set-key (kbd "s-1") #'(lambda () (interactive) (persp-switch-by-number 1)))
+(global-set-key (kbd "s-2") #'(lambda () (interactive) (persp-switch-by-number 2)))
+(global-set-key (kbd "s-3") #'(lambda () (interactive) (persp-switch-by-number 3)))
+(global-set-key (kbd "s-4") #'(lambda () (interactive) (persp-switch-by-number 4)))
+(global-set-key (kbd "s-5") #'(lambda () (interactive) (persp-switch-by-number 5)))
+(global-set-key (kbd "s-6") #'(lambda () (interactive) (persp-switch-by-number 6)))
+(global-set-key (kbd "s-7") #'(lambda () (interactive) (persp-switch-by-number 7)))
+(global-set-key (kbd "s-8") #'(lambda () (interactive) (persp-switch-by-number 8)))
+(global-set-key (kbd "s-9") #'(lambda () (interactive) (persp-switch-by-number 9)))
 
 (provide 'init-workspace)
 ;;; init-workspace.el ends here
