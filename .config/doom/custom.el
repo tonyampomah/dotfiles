d4bf419e01a6d7e91925e163160d09ef97f397a0
Tony Ampomah <tony@arksolutions.it>
2026-06-18 09:38:10 +0100

    Aerospace keybinding tweak

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("7771c8496c10162220af0ca7b7e61459cb42d18c35ce272a63461c0fc1336015" default)))
+   '("5c8a1b64431e03387348270f50470f64e28dfae0084d33108c33a81c1e126ad6"
+     "7771c8496c10162220af0ca7b7e61459cb42d18c35ce272a63461c0fc1336015" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
