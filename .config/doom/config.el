fb5e37cbc57570aed68a346a6b512a3845a4df38
Tony Ampomah <tony@arksolutions.it>
2026-06-24 23:28:19 +0100

    Add ox-latex tweaks for doom emacs

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -203,6 +203,20 @@
      ))
 
 
+  (with-eval-after-load 'ox-latex
+    (add-to-list 'org-latex-classes
+                 '("org-plain-latex"
+		   "\\documentclass{article}
+           [NO-DEFAULT-PACKAGES]
+           [PACKAGES]
+           [EXTRA]"
+		   ("\\section{%s}" . "\\section*{%s}")
+		   ("\\subsection{%s}" . "\\subsection*{%s}")
+		   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+		   ("\\paragraph{%s}" . "\\paragraph*{%s}")
+		   ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+
+
   (setq org-refile-targets
         '((nil :maxlevel . 3)
 	  (org-agenda-files :maxlevel . 3))
